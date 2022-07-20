3d83322e907ded82f2fe01ce9d3bc5fb78bd6042
Bindu <Bindu@192.168.0.22>
2022-07-20 12:46:46 -0400

    Initial framework created

---
+++ ClipboardAmazon/src/main/java/ClipboardAmazon/ClipboardAmazon/App.java
@@ -0,0 +1,13 @@
+package ClipboardAmazon.ClipboardAmazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
